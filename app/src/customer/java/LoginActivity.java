29f5aad6c32ad9ce6bbb9d15b5dedb66f7209f07
“Raja <rajajawahar77@gmail.com>
2018-01-09 13:14:25 +0530

    Initial Commit

---
+++ app/src/customer/java/LoginActivity.java
@@ -0,0 +1,16 @@
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+/**
+ * Created by rajajawahar on 09/01/18.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Toast.makeText(this, "Customer Login Activity", Toast.LENGTH_SHORT).show();
+  }
+}
